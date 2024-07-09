0c02912363154e8c8f8b248496998f9dbdbaef0e
tamas <>
2024-07-09 05:01:54 -0400

    Employee

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Operations;
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Employee quittable;
+
+        // Create an object of type Employee and assign it to the IQuittable variable
+        quittable = new Employee();
+        quittable.Quit();
+
+    }
+}
+
